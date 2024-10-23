51d3d1e4e352cb764765058b3b253f5630c45728
jeffery-zhang <jefferyzhang1013@gmail.com>
2024-10-23 13:24:32 +0800

    add edge runtime in route

--- app/api/ipinfo/route.ts
+++ app/api/ipinfo/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from 'next/server'
 
 import { getIpInfo, IpInfoResponse } from './api'
 
+export const runtime = 'edge'
+
 export async function GET(
   req: NextRequest,
 ): Promise<NextResponse<IpInfoResponse>> {
